0ed159e2596bc930f5e9d99a8cb866c15338bd78
Wiktor Kowalski <wiktor9196667@gmail.com>
2021-10-13 20:22:07 +0200

    Feature/docker (#3)
    * feature: add dockerfile
    
    * feature: add docker-compose
    
    * feature: add example menu entries

---
+++ src/menu/exampleMenuItems.ts
@@ -0,0 +1,28 @@
+import { MenuItem } from "./menuitem.entity";
+
+export const exampleMenuItems: MenuItem[] = [
+    {
+        id: 1,
+        category: "Pizza",
+        name: "Calzone",
+        price: 10
+    },
+    {
+        id: 2,
+        category: "Pizza",
+        name: "Peperoni",
+        price: 15
+    },
+    {
+        id: 3,
+        category: "Pizza",
+        name: "Capriciosa",
+        price: 16
+    },
+    {
+        id: 4,
+        category: "Pizza",
+        name: "Margherita",
+        price: 11
+    },
+];
--- src/menu/menu.service.ts
+++ src/menu/menu.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Providers } from 'src/providers';
 import { Repository } from 'typeorm';
+import { exampleMenuItems } from './exampleMenuItems';
 import { MenuItem } from './menuitem.entity';
 
 @Injectable()
@@ -8,7 +9,9 @@ export class MenuService {
   constructor(
     @Inject(Providers.MenuRepository)
     private readonly menuRepository: Repository<MenuItem>,
-  ) { }
+  ) {
+    this.seedMenu();
+  }
 
   async findAll(): Promise<MenuItem[]> {
     return await this.menuRepository.find();
@@ -16,5 +19,12 @@ export class MenuService {
 
   async findOne(id: string): Promise<MenuItem> {
     return await this.menuRepository.findOne(id);
+  }
+
+  async seedMenu() {
+    if (await this.menuRepository.count() === 0) {
+      this.menuRepository.save(exampleMenuItems);
+      console.debug('MenuItem table content was autofilled');
+    }
   }
 }
